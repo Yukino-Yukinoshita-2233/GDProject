8c2f31136b95d5e0675523e460891b5873a5867c
Yukino-Yukinoshit-2233 <zero2410465558@outlook.com>
2024-11-02 14:45:58 +0800

    添加两个资源

--- Assets/Script/Map/MapGenerator.cs
+++ Assets/Script/Map/MapGenerator.cs
@@ -11,51 +11,33 @@ public class MapGenerator : MonoBehaviour
     public int height = 100;
     public float scale = 20f;
 
+
     // 基础地形预制件
     public GameObject grassPrefab;  //草地形
     public GameObject waterPrefab;  //水地形
     public float waterTerrainSize = 0.3f;   //水地形占位百分比
 
-    //资源地形预制件
-    public GameObject forestPrefab; //森林资源地形
-    public GameObject minePrefab;   //矿物资源地形
-    public GameObject farmPrefab;   //食物资源地形
-    
-    public int forestResourcePosCount = 10;    //森林资源点数量
-    public int forestresourceBlockSize = 5;   //森林资源点大小
-    //public int mineResourcePosCount = 10;    //矿物资源点数量
-    //public int mineresourceBlockSize = 5;   //矿物资源点大小
-    //public int farmResourcePosCount = 10;    //食物资源点数量
-    //public int farmresourceBlockSize = 5;   //食物资源点大小
-
-
-    //// 建筑预制件
-    //public GameObject baseCampPrefab;
-    //public GameObject wallPrefab;
-    //public GameObject towerPrefab;
-    //public GameObject housePrefab;
-    //public GameObject woodFactoryPrefab;
-    //public GameObject stoneFactoryPrefab;
-    //public GameObject metalFactoryPrefab;
+    public GameObject[] resourcePrefabs; // 资源预制体数组
+    public int[] resourceScale;// 资源大小
+    public float resourcesTerrainSize = 0.3f;   //资源地形占位百分比
 
     // 节点网格
     private GridNode[,] mapGrid;
-    private GridNode[,] ResourcesGrid;
+    bool isWalkable = true;
+
     // 噪声地图
     private float[,] noiseMap;
-    // 噪声偏移值
-    private Vector2 offset;
 
     void Start()
     {
         GenerateMap();  //生成地形
+
     }
 
     [ContextMenu("TestMap")]
     private void TestMap()
     {
         GenerateMap();
-
     }
 
     // 生成地图数据二维数组
@@ -63,10 +45,13 @@ public class MapGenerator : MonoBehaviour
     {
         noiseMap = GenerateNoiseMap(width, height, scale);
         mapGrid = new GridNode[width, height];
-        PlaceResources();
 
-                CreateMap();
+        CreateMap();
 
+        for (int i = 0; i < resourcePrefabs.Length; i++)
+        {
+            GenerateResources(i);
+        }
     }
 
     //创建地形并保存到节点网格grid
@@ -86,7 +71,7 @@ public class MapGenerator : MonoBehaviour
             {
                 float sample = noiseMap[x, z];
                 Vector3 position = new Vector3(x, 0, z);
-                bool isWalkable = true;
+                isWalkable = true;
                 GameObject terrainPrefab = null;
 
                 // 根据噪声值选择地形
@@ -99,25 +84,10 @@ public class MapGenerator : MonoBehaviour
                     isWalkable = false; // 河流不可通行
 
                 }
-                else if(sample >= waterTerrainSize &&  sample <= 1.0f)
+                else
                 {
                     terrainPrefab = grassPrefab;
 
-                }
-                else if (sample == 2.0f)
-                {
-                    terrainPrefab = forestPrefab;
-                    isWalkable = false;
-                }
-                else if (sample == 3.0f)
-                {
-                    terrainPrefab = minePrefab;
-                    isWalkable = false;
-                }
-                else if (sample == 4.0f)
-                {
-                    terrainPrefab = farmPrefab;
-                    isWalkable = false;
                 }
 
 
@@ -132,71 +102,56 @@ public class MapGenerator : MonoBehaviour
 
     }
 
-
     // 生成噪声地图
     float[,] GenerateNoiseMap(int width, int height, float scale)
     {
         // 生成噪声偏移值
-        offset = new Vector2(Random.Range(0f, 1000f), Random.Range(0f, 1000f));
+        Vector2 offset = new Vector2(Random.Range(0f, 1000f), Random.Range(0f, 1000f));
 
         float[,] noiseMap = new float[width, height];
 
         for (int x = 0; x < width; x++)
         {
-            for (int z = 0; z < height; z++)
+            for (int y = 0; y < height; y++)
             {
                 float sampleX = (x + offset.x) / scale;
-                float sampleZ = (z + offset.y) / scale;
+                float sampleZ = (y + offset.y) / scale;
                 float noiseValue = Mathf.PerlinNoise(sampleX, sampleZ);
-                noiseMap[x, z] = noiseValue;
+                noiseMap[x, y] = noiseValue;
             }
         }
 
-        return noiseMap;
+        return noiseMap;   
     }
 
-    // 在草地上获取随机位置
-    Vector3 GetRandomPositionOnGrass()
+    void GenerateResources(int resourceIndex)
     {
-        while (true)
-        {
-            int x = Random.Range(10, width-10);
-            int z = Random.Range(10, height-10);
-            if (noiseMap[x, z] >= waterTerrainSize && noiseMap[x, z] <= 1.0f)
-            {
-                return new Vector3(x, 0, z);
-            }
-        }
-    }
+        // 生成噪声偏移值
+        Vector2 offset = new Vector2(Random.Range(0f, 1000f), Random.Range(0f, 1000f));
 
-    // 随机放置障碍物
-    void PlaceResources()
-    {
-        for (int i = 0; i < forestResourcePosCount; i++)
+        for (int x = 0; x < width; x++)
         {
-
-            Vector3 position = GetRandomPositionOnGrass();
-            int newforestresourceBlockSize = 0;
-            // 遍历资源区块内的每个点
-            for (int x = ((int)position.x - newforestresourceBlockSize/2); x <= (position.x + newforestresourceBlockSize/2); x++)
+            for (int y = 0; y < height; y++)
             {
-                for (int z = ((int)position.z- newforestresourceBlockSize/2); z <= (position.z + newforestresourceBlockSize/2); z++)
+                GridNode node = mapGrid[x, y];
+
+                if (node.IsWalkable) // 确保资源只生成在可行走区域
                 {
-                    // 在噪声地图上标记资源区块（例如，将高度值增加或设置为特定值）
-                    noiseMap[x, z] = Random.Range(2,5); // 将资源区块的高度设置为最大值
+                    float xCoord = ((float)x + offset.x) / width * resourceScale[resourceIndex];
+                    float yCoord = ((float)y + offset.y) / height * resourceScale[resourceIndex];
+                    float sample = Mathf.PerlinNoise(xCoord, yCoord);
+
+                    if (sample > resourcesTerrainSize) // 设定阈值，表示大于resourcesTerrainSize的点生成资源
+                    {
+                        isWalkable = false;
+                        Vector3 position = new Vector3(x, 1, y);
+                        GameObject resourceInstance = Instantiate(resourcePrefabs[resourceIndex], position, Quaternion.identity);
+                        resourceInstance.transform.parent = transform;
+                        mapGrid[x, y] = new GridNode(resourceInstance, position, isWalkable, x, y);
+
+                    }
                 }
             }
         }
     }
-
-    //// 随机放置建筑
-    //void PlaceBuildings()
-    //{
-    //    for (int i = 0; i < 10; i++)
-    //    {
-    //        Vector3 position = GetRandomPositionOnGrass();
-    //        Instantiate(baseCampPrefab, position, Quaternion.identity);
-    //    }
-    //}
-
 }
